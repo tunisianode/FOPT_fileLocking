ae2b2f0e108f0a8871be83edfb21064cd6510a02
tunisianode <tunisianode@gmail.com>
2017-11-21 22:19:07 +0100

    initial Commit

--- filelocking/File.java
+++ filelocking/File.java
@@ -3,6 +3,8 @@ package pp.filelocking;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//todo: muss versuchen mit Likedlist dieser aufgabe zu lösen
 public class File extends Thread
 {
     //Attribute:
@@ -23,32 +25,26 @@ public class File extends Thread
 
     public static void main(String[] args)
     {
-
-
-        File f1 = new File(100;
-        //File f2 = new File(2048);
-        f1.start();
-        f1.lock(50, 60);
-        f1 f1.lock(61, 75);
-        f1.lock(75, 15);
-        f1.unlock(50, 60);
+        File file1 = new File(100);
+        file1.start();
+        file1.lock(50, 60);
+        file1.lock(61, 75);
+        file1.lock(75, 80);
+        file1.unlock(50, 60);
     }
 
     public synchronized void lock(int begin, int end)
     {
         if (begin < 0 || end < 0 || begin > end || end > length)
         {
-            throw new IllegalArgumentException("Error bei Lock methode:  begin < 0 || end < 0 || begin > end || end "
-                    + ">" + " length");
+            throw new IllegalArgumentException("Error bei Lock methode:  begin < 0 || end < 0 || begin > end || end " + ">" + " length");
         }
 
         Intervall intervall = new Intervall(begin, end);
 
         for (Intervall item : IntervallList)
         {
-            if ((intervall.getStart() >= item.getStart() && intervall.getEnd() <= item.getEnd()) || (intervall
-                    .getStart() >= item.getStart() && intervall.getStart() <= item.getEnd()) || (intervall.getEnd()
-                    >= item.getStart() && intervall.getEnd() <= item.getEnd()))
+            if ((intervall.getStart() >= item.getStart() && intervall.getEnd() <= item.getEnd()) || (intervall.getStart() >= item.getStart() && intervall.getStart() <= item.getEnd()) || (intervall.getEnd() >= item.getStart() && intervall.getEnd() <= item.getEnd()))
             {
                 //throw new IllegalArgumentException("Uberlappung bei das Intervall [" + begin + "," + end + "]");
                 //                          Uberlappung
@@ -72,8 +68,13 @@ public class File extends Thread
         //auserhalb der bereich
         if (begin < 0 || end < 0 || begin > end || end > length)
         {
-            throw new IllegalArgumentException("Error bei Lock methode:  begin < 0 || end < 0 || begin > end || end "
-                    + ">" + " length");
+            throw new IllegalArgumentException("Error bei Lock methode:  begin < 0 " + "|| end < 0 || begin > end || " +
+                    "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + ""
+                    + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" +
+                    "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + ""
+                    + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" +
+                    "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "" + "end "
+                    + "" + "" + ">" + " " + "" + "" + "length");
         }
 
 
@@ -87,6 +88,7 @@ public class File extends Thread
     }
 
     //prüft die Gültichkeit von ein eingegebene Intervall
+    //todo: der check muss ausgelagert werden
     private Boolean IsValid()
     {
         return true;
@@ -103,7 +105,7 @@ class Intervall
     {
         this.start = start;
         this.end = end;
-        this.isLocked = false;
+        this.isLocked = true;
     }
 
     public void setLocked(Boolean locked)
